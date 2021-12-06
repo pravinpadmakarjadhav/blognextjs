8d3d3230608e60fd30e09ab93d3a3af49c15c1de
Pravin Jadhav <83411817+pravinpadmakarjadhav@users.noreply.github.com>
2021-12-06 09:33:49 +0530

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
-export default function Layout() {
+import Head from "next/head"
+import Header from "./Header"
+export default function Layout({title,keywords,description,children}) {
     return (
         <div>
-            
+        <Head>
+        
+        
+        <title>{title}</title>
+        <meta name="keywords" content={keywords}/>
+        <meta name="description" content={description}/>
+        <link rel ="icon" href="/favicon.ico"/>
+        </Head>    
+<Header/>
+<main className='container mx-auto my-7'>{children}</main>
         </div>
     )
+}
+Layout.defaultProps = {
+
+    title:"welcome",
+    keywords:"dev coding cloud",
+    description:"best info",
+
 }
